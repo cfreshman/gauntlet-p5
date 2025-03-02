bac20419cb47d7ab0a12de43667dec2445eb1901
freshman <cyrus@freshman.dev>
2025-03-02 12:48:01 -0600

    cleanup

--- src/layer3/musicAgent.js
+++ src/layer3/musicAgent.js
@@ -232,6 +232,7 @@ TIPS:
 - if the user wants a play session, you can either queue tracks or create a playlist and then queue that
 - DO NOT RETURN LINKS AND SAY 'listen on Spotify' unless the user asks for that. you can just return linked resources, and suggest queueing or creating a playlist
 - you don't have to use the link conversion tool if you already have artist & track names. just targeted search on Spotify
+- be smart and creative. if the user wants mood playlists - search for different moods, don't just look up 'mood playlist'
 
 YOUR MAIN TASK IN THE FIRST TURN IS TO CREATE A PLAN ON HOW TO SATISFY THE USER REQUEST (unless the user is just chatting)
 COMPLETE YOUR GOAL. DO NOT RETURN PARTIAL RESULTS. e.g. A PLAYLIST MUST HAVE ALL 30+ SONGS ADDED
@@ -241,7 +242,7 @@ DO NOT RETURN SONG LINKS AND TELL THE USER TO CLICK THEM. it makes more sense to
 ALWAYS SAY HOW MANY SONGS OR WHATEVER YOU'VE ADDED OR DONE ANYTHING WITH. THE USER WANTS TO KNOW
 DON'T FORGET THE ACTUAL USER REQUEST
 DO NOT SAY "LISTEN ON SPOIFY"
-**FINAL WORD: DO THINGS THE HUMAN WILL LIKE. AND BE CONCISE**`
+**DO THINGS THE HUMAN WILL LIKE. AND BE CONCISE**`
           }
         ];
 
